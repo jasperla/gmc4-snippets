449fc148aae66645da26df546cedaea10710c7a5
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2011-08-26 15:11:42 +0200

    Display the pressed key using the 2-pin LEDs

---
+++ show_key.s
@@ -0,0 +1,10 @@
+; Display the pressed key using the 2-pin LEDs.
+
+0x00	0	; KA
+0x01	AE	; TIY[E]
+0x03	4	; AM
+0x04	AF	; TIY[F]
+0x06	80	; TIA[0]
+0x08	4	; AM
+0x09	ED	; CALL SND
+0x0B	F00	; JUMP[0][0]
