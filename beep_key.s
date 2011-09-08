39f7cf08e3924716445374b58824a2b11b490e2f
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2011-09-08 23:49:13 +0200

    Two small nits, as found by my assembler gmc4-as (https://github.com/jasperla/gmc4-as)

--- beep_key.s
+++ beep_key.s
@@ -1,5 +1,5 @@
 ; Beep according to the pressed key
 
 0x00	0	; KA
-0x01	EB	; CALL SUND
+0x01	EB	; CAL SUND
 0x03	F0	; JUMP[0]
--- show_key.s
+++ show_key.s
@@ -6,5 +6,5 @@
 0x04	AF	; TIY[F]
 0x06	80	; TIA[0]
 0x08	4	; AM
-0x09	ED	; CALL SND
+0x09	ED	; CAL SUND
 0x0B	F00	; JUMP[0][0]
